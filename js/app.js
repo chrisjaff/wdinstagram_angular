31e1d5b9c9e7b9e97f603efc76937961c00ee9fe
Chris Jaffier <chrisd@Chriss-MacBook-Pro.local>
2016-10-27 08:48:46 -0400

    made app w/index ctrl

--- js/app.js
+++ js/app.js
@@ -1,5 +1,46 @@
 "use strict";
 
-(function(){
-  angular.module("wdinstagram", [])
-})();
+//(function(){
+  //angular.module("wdinstagram", [])
+//})();
+
+
+let postData = [
+  {entry: "Testing", photo_url: "www.msn.com", author: "Pop", body:"post"},
+  {entry: "Testings", photo_url: "www.youtube.com", author: "Raw", body:"post"}
+]
+
+
+angular
+.module("wdinstagram", [
+    "ui.router"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ])
+  .controller("GrumbleIndexController", [
+    GrumbleIndexControllerFunction
+  ]);
+
+  function GrumbleIndexControllerFunction(){
+    this.wdinstagrams = postData
+    this.showWdinstigram = function() {
+    let wdinstagrams = {entry: this.newWdinstagram }
+
+  }
+
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("wdinstagramIndex", {
+      url: "/wdinstagrams",
+      templateUrl: "js/ng-views/index.html",
+      controller: "wdinstagramIndexController",
+      controllerAs: "vm"
+    })
+    .state("wdinstagramShow", {
+      url: "/wdinstagrams/:id",
+      templateUrl: "js/ng-views/show.html"
+    });
+  }
+}
